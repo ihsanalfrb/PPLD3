a51dbe3bcfa64e892e01848ccb0a284f10e7aac6
mihp26 <m.ihp@icloud.com>
2017-03-17 14:11:48 +0700

    create tag batik table

--- database/migrations/2017_03_16_083221_create_tag_batik_table.php
+++ database/migrations/2017_03_16_083221_create_tag_batik_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CrateTagBatikTable extends Migration
+class CreateTagBatikTable extends Migration
 {
     /**
      * Run the migrations.
