e525770a7db8c57f519cc95b80c3f8e879d66f3e
mihp26 <m.ihp@icloud.com>
2017-03-17 14:01:37 +0700

    create pola batik table

---
+++ database/migrations/2017_03_16_083200_create_pola_batik_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CratePolaBatikTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pola_batik', function (Blueprint $table) {
+            $table->string('id_batik');
+            $table->string('tag');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pola_batik');
+    }
+}
