28ba04627ac952b8fa419f357138af99cd0f6272
mihp26 <m.ihp@icloud.com>
2017-03-17 14:11:37 +0700

    create tag batik table

--- database/migrations/2017_03_16_083200_create_pola_batik_table.php
+++ database/migrations/2017_03_16_083200_create_pola_batik_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CratePolaBatikTable extends Migration
+class CreatePolaBatikTable extends Migration
 {
     /**
      * Run the migrations.
