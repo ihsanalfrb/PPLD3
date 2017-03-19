4d738a5437425ef3e29ccf5ebeba6612b741a482
Rizqy Faishal <rizqyfaishal27@gmail.com>
2017-03-19 22:51:16 +0700

    update gitlab-ci.yml and gitlab-ci.sh

--- .gitlab-ci.sh
+++ .gitlab-ci.sh
@@ -5,11 +5,16 @@
 # set -xe
 
 # Update packages and install composer and PHP dependencies.
-apt-get update -yqq
-apt-get install git php5-xdebug libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev -yqq
+# apt-get update -yqq
+# apt-get install git php5-xdebug libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev -yqq
 
-# Compile PHP, include these extensions.
-docker-php-ext-install mbstring mcrypt pdo_mysql curl json intl gd xml zip bz2 opcache
+# # Compile PHP, include these extensions.
+# docker-php-ext-install mbstring mcrypt pdo_mysql curl json intl gd xml zip bz2 opcache
+
+echo "zend_extension=\"/usr/lib/php5/20100525/xdebug.so\"" >> /etc/php5/apache2/php.ini 
+echo "xdebug.remote_enable=1" >> /etc/php5/apache2/php.ini 
+echo "xdebug.remote_handler=dbgp xdebug.remote_mode=req" >> /etc/php5/apache2/php.ini 
+echo "xdebug.remote_host=127.0.0.1 xdebug.remote_port=9000" >> /etc/php5/apache2/php.ini
 
 # Install Composer and project dependencies.
 curl -sS https://getcomposer.org/installer | php
