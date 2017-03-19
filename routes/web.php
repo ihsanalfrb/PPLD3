442af90b3f085264ba48bd5cc31ea855ed53114a
Rizqy Faishal <rizqyfaishal27@gmail.com>
2017-03-19 16:21:47 +0700

    fix broken routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< .merge_file_Urg84S
 
 /*
 |--------------------------------------------------------------------------
@@ -15,11 +14,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-=======
-/**
- * Created by PhpStorm.
- * User: Ivan
- * Date: 3/17/17
- * Time: 2:32 PM
- */
->>>>>>> .merge_file_A9iGM5
