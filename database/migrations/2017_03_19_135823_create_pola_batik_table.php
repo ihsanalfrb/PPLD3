9f88dca7ffbe60915968cbac7376d1449f74ede7
mihp26 <m.ihp@icloud.com>
2017-03-19 21:01:23 +0700

    create batiks, pola_batik, tag_batik table migration

--- database/migrations/2017_03_19_135823_create_pola_batik_table.php
+++ database/migrations/2017_03_19_135823_create_pola_batik_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CrateBatiksTable extends Migration
+class CreatePolaBatikTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CrateBatiksTable extends Migration
      */
     public function up()
     {
-        Schema::create('batiks', function (Blueprint $table) {
+        Schema::create('pola_batik', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nama_batik');
-            $table->text('makna_batik');
-            $table->text('sejarah_batik');
-            $table->string('asal_daerah');
+            $table->timestamps();
         });
     }
 
@@ -29,6 +26,6 @@ class CrateBatiksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('batiks');
+        Schema::dropIfExists('pola_batik');
     }
 }
