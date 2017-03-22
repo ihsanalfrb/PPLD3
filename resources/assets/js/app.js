4bac2653d6ff774cd2731785fdfc45995f85ab0d
Rizqy Faishal <rizqyfaishal27@gmail.com>
2017-03-23 00:58:33 +0700

    update navbar -> responsive

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,1 +1,12 @@
-console.log('Batique Javascript');
+jQuery(document).ready(function ($) {
+
+  var $toggle = $('#nav-toggle');
+  var $menu = $('#nav-menu');
+
+  $toggle.click(function() {
+    $(this).toggleClass('is-active');
+    $menu.toggleClass('is-active');
+  });
+
+
+});
--- resources/views/components/_navbar.blade.php
+++ resources/views/components/_navbar.blade.php
@@ -1,12 +1,16 @@
-<nav class="nav">
+<nav class="nav has-shadow">
   <div class="container">
     <div class="nav-left">
         <a class="nav-item is-brand" href="http://bulma.io">
           <img src="{{ URL::asset('images/logo.png')}}" alt="Bulma: a modern CSS framework based on Flexbox">
         </a>
+        <a class="nav-item is-tab is-hidden-mobile is-active" href="#">Knowledge</a>
+        <a class="nav-item is-tab is-hidden-mobile" href="#">Forums</a>
+        <a class="nav-item is-tab is-hidden-mobile" href="#">Learn More</a>
+        <a class="nav-item is-tab is-hidden-mobile" href="#">About Us</a>
       </div>
       <div class="nav-center">
-        <a class="nav-item" href="https://github.com/jgthms/bulma">
+        <a class="nav-item" href="https://github.com/rizqyfaishal">
           <span class="icon">
             <i class="fa fa-github"></i>
           </span>
@@ -23,32 +27,17 @@
       <span></span>
       </span>
       <div id="nav-menu" class="nav-right nav-menu">
-        <a class="nav-item is-active" href="http://bulma.io/">Home</a>
-        <a class="is-hidden nav-item  " href="http://bulma.io/templates/">
-          <span>Templates</span>
-          <span class="tag is-small is-success">New!</span>
-        </a>
-        <a class="nav-item  " href="/documentation/overview/start/">Documentation</a>
-        <a class="nav-item " href="http://bulma.io/blog/">Blog</a>
         <span class="nav-item">
-          <a id="twitter"
-            class="button"
-            data-social-network="Twitter"
-            data-social-action="tweet"
-            data-social-target="http://bulma.io"
-            target="_blank"
-            href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&url=http://bulma.io&via=jgthms">
-            <span class="icon">
-              <i class="fa fa-twitter"></i>
-            </span>
-            <span>Tweet</span>
-          </a>
-          <a class="button is-primary" href="https://github.com/jgthms/bulma/archive/0.4.0.zip">
-            <span class="icon">
-              <i class="fa fa-download"></i>
-            </span>
-          <span>Download</span>
-          </a>
+          <div class="field is-grouped">
+            <p class="control is-expanded">
+              <input class="input" type="text" placeholder="Find a batik">
+            </p>
+            <p class="control">
+              <a class="button is-primary">
+                Search
+              </a>
+            </p>
+          </div>
         </span>
       </div>
   </div>    
