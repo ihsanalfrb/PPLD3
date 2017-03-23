1d464c9b12a00391977f943779fad215ac44f36d
Geraldo <geraldo@GW.z>
2017-03-23 10:10:24 +0700

    add query to retrieve batik information from DB

--- app/Http/Controllers/RincianInfoBatikController.php
+++ app/Http/Controllers/RincianInfoBatikController.php
@@ -82,4 +82,37 @@ class RincianInfoBatikController extends Controller
     {
         //
     }
+
+    
+    /* Function that will match batik by its distinct value,
+        will return array of batik information
+    @param  batikIDs collection of id processed by identifyUserImageResult(implemented later)
+
+    */
+    function getBatikInfoFromDB($batikIDs){
+        foreach ($batikIDs as $id) {
+            $batikInfoRAW = DB::table('batik')->where('id', $id)->first();
+            //deprecated
+            // $result = $result->merge($batikInfoRAW);
+            $result->push($batikInfoRAW);
+        }
+
+        if ($result->count() > 0){ // If there is releated batik
+            echo Response::json($result);
+        }
+        else{
+            // default : no object is shown as there is no related batik
+            // move to another landing page
+        }
+
+
+    }
+    /* Function that will receive image/imageURL from pattern recognition machine
+    and resolve it to array of id
+    */
+    function identifyRelatedBatikResult($batikObjects){
+        $resolvedIDs;
+        // later implementation
+        return getBatikInfoFromDB($resolvedIDs);                    
+    }
 }
