4336d2da9aee1157cde8db7b2858402681685865
Rizqy Faishal <rizqyfaishal27@gmail.com>
2017-03-28 13:42:43 +0700

    add model factory for testing

--- database/factories/PolaBatikFactory.php
+++ database/factories/PolaBatikFactory.php
@@ -12,13 +12,10 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-// $factory->define(App\User::class, function (Faker\Generator $faker) {
-//     static $password;
+$factory->define(App\PolaBatik::class, function (Faker\Generator $faker) {
 
-//     return [
-//         'name' => $faker->name,
-//         'email' => $faker->unique()->safeEmail,
-//         'password' => $password ?: $password = bcrypt('secret'),
-//         'remember_token' => str_random(10),
-//     ];
-// });
+    return [
+        'gambar_pola_batik' => $faker->address,
+        'matriks_pola_batik' => $faker->address
+    ];
+});
