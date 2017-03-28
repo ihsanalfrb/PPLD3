bfce97ae0c3184f3f9d9d4d0bed7974a286fb879
Ivan Halim <m.ihp@icloud.com>
2017-03-28 22:12:24 +0700

    insert batik_tag_batik relation

--- database/seeds/TagBatikTableSeeder.php
+++ database/seeds/TagBatikTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Batik;
+use App\TagBatik;
 use Illuminate\Database\Seeder;
 
 class TagBatikTableSeeder extends Seeder
@@ -12,18 +14,195 @@ class TagBatikTableSeeder extends Seeder
     public function run()
     {
         //
-        DB::table('tag_batik')->insert([
-            array('id_batik' => '', 'tag_batik' => ''),
-            array('id_batik' => '', 'tag_batik' => ''),
-            array('id_batik' => '', 'tag_batik' => ''),
-            array('id_batik' => '', 'tag_batik' => ''),
-            array('id_batik' => '', 'tag_batik' => ''),
-            array('id_batik' => '', 'tag_batik' => ''),
-            array('id_batik' => '', 'tag_batik' => ''),
-            array('id_batik' => '', 'tag_batik' => ''),
-            array('id_batik' => '', 'tag_batik' => ''),
-            array('id_batik' => '', 'tag_batik' => ''),
-            array('id_batik' => '', 'tag_batik' => '')
-        ]);
+//        DB::table('tag_batik')->insert([
+//            array('id_batik' => '', 'tag_batik' => ''),
+//            array('id_batik' => '', 'tag_batik' => ''),
+//            array('id_batik' => '', 'tag_batik' => ''),
+//            array('id_batik' => '', 'tag_batik' => ''),
+//            array('id_batik' => '', 'tag_batik' => ''),
+//            array('id_batik' => '', 'tag_batik' => ''),
+//            array('id_batik' => '', 'tag_batik' => ''),
+//            array('id_batik' => '', 'tag_batik' => ''),
+//            array('id_batik' => '', 'tag_batik' => ''),
+//            array('id_batik' => '', 'tag_batik' => ''),
+//            array('id_batik' => '', 'tag_batik' => '')
+//        ]);
+
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+        $this->create_tag_batik_model('', '');
+    }
+
+    public function create_tag_batik_model($tag, $namabatik) {
+        $tagbatik = new TagBatik();
+
+        $tagbatik->tag_batik = $tag;
+        $tagbatik->save() ;
+
+        $idbatik = App\Batik::where('nama_batik','=',$namabatik)->first()->pluck('id');
+        $idtagbatik = App\TagBatik::where('tag_batik','=',$tag)->first()->pluck('id');
+        DB::table('batik_pola_batik')->insert(array('batik_id'=>$idbatik, 'pola_batik_id'=>$idtagbatik));
     }
 }
