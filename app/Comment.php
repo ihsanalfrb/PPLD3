450ef0a10eb4c233ec7dec41355d7c1b855c21bb
mihp26 <m.ihp@icloud.com>
2017-04-01 00:13:23 +0700

    create useraccount model

--- app/Comment.php
+++ app/Comment.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     //
+    use SoftDeletes;
+
+    protected $table = 'comments';
+    protected $fillable = ['judul_komentar','isi_komentar','comment_by','thread_id', 'created_at'];
+    protected $dates = ['delete_at'];
 }
