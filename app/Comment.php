5ae88607bd02eb2f3b3a4f7882e983f10e4e68a9
mihp26 <m.ihp@icloud.com>
2017-04-01 01:39:03 +0700

    add relation to thread and useraccount

--- app/Comment.php
+++ app/Comment.php
@@ -12,4 +12,12 @@ class Comment extends Model
     protected $table = 'comments';
     protected $fillable = ['judul_komentar','isi_komentar','comment_by','thread_id', 'created_at'];
     protected $dates = ['delete_at'];
+
+    public function in_thread() {
+        return $this->belongsTo('App\Thread', 'thread_id');
+    }
+
+    public function comment_by() {
+        return $this->belongsTo('App\UserAccounts','comment_by');
+    }
 }
