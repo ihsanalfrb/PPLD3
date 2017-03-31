52dc11e9d31dd20eff6ae3802d167e6697b1cab9
mihp26 <m.ihp@icloud.com>
2017-04-01 00:14:31 +0700

    create useraccount model

--- app/UserAccount.php
+++ app/UserAccount.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserAccount extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $table = 'user_accounts';
+    protected $fillable = ['email','username','password','nama', 'tanggal_lahir', 'jenis_kelamin', 'created_at'];
+    protected $dates = ['delete_at'];
 }
