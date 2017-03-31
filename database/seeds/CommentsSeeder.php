089444fc901507afca0d1b825f3b2f50667753c6
mihp26 <m.ihp@icloud.com>
2017-04-01 02:33:14 +0700

    create comment, thread and useraccount seeder

---
+++ database/seeds/CommentsSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Comment;
+use Illuminate\Database\Seeder;
+
+class CommentsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+
+    public function create_comment_model($judulkomentar, $isikomentar, $commentby, $threadid) {
+
+        $comment = new Comment();
+        $comment->judul_komentar = $judulkomentar;
+        $comment->isi_komentar = $isikomentar;
+        $comment->comment_by = $commentby;
+        $comment->thread_id = $threadid;
+        $comment->save();
+
+//        $idbatik = App\Batik::where('nama_batik','=',$namabatik)->first()->id;
+//        $idtagbatik = App\TagBatik::where('tag_batik','=',$tag)->first()->id;
+//
+//        DB::table('batik_tag_batik')->insert(array('batik_id'=>$idbatik, 'tag_batik_id'=>$idtagbatik));
+    }
+}
---
+++ database/seeds/ThreadsSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Thread;
+use Illuminate\Database\Seeder;
+
+class ThreadsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+
+    public function create_thread_model($namathread, $createdby, $views, $replies) {
+        $thread = new Thread();
+        $thread->nama_thread = $namathread;
+        $thread->created_by = $createdby;
+        $thread->views = $views;
+        $thread->replies = $replies;
+        $thread->save();
+
+//        $idbatik = App\Batik::where('nama_batik','=',$namabatik)->first()->id;
+//        $idtagbatik = App\TagBatik::where('tag_batik','=',$tag)->first()->id;
+//
+//        DB::table('batik_tag_batik')->insert(array('batik_id'=>$idbatik, 'tag_batik_id'=>$idtagbatik));
+    }
+}
