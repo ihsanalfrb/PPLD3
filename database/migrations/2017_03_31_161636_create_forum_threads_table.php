edfcb2a3641a899173b96d3506cf911d07571c4e
mihp26 <m.ihp@icloud.com>
2017-04-04 12:09:40 +0700

    fix table name

--- database/migrations/2017_03_31_161636_create_forum_threads_table.php
+++ database/migrations/2017_03_31_161636_create_forum_threads_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateThreadsTable extends Migration
+class CreateForumThreadsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateThreadsTable extends Migration
      */
     public function up()
     {
-        Schema::create('threads', function (Blueprint $table) {
+        Schema::create('forum_threads', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nama_thread');
             $table->string('created_by');
@@ -30,6 +30,6 @@ class CreateThreadsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('ThreadsSeeder');
+        Schema::dropIfExists('forum_threads');
     }
 }
