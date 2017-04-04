20a5854e5dcb2db6b0f14950a6098d74e98741cd
mihp26 <m.ihp@icloud.com>
2017-04-04 13:01:05 +0700

    create comment factory

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ivan
+ * Date: 4/4/17
+ * Time: 12:44 PM
+ */
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Batik::class, function (Faker\Generator $faker) {
+    return [
+        'judul_komentar' => $faker->title,
+        'isi_komentar' => $faker->paragraph,
+        'comment_by' => $faker->randomDigitNotNull,
+        'thread_id' => $faker->randomDigitNotNull
+    ];
+});
