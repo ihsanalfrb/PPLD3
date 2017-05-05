ac75566ca832422b76acaad29c1f28848e0684ea
mihp26 <m.ihp@icloud.com>
2017-05-05 23:06:10 +0700

    [red] add show profile page response test

--- tests/Feature/ShowProfileTest.php
+++ tests/Feature/ShowProfileTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -9,13 +10,31 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ShowProfileTest extends TestCase
 {
+    use DatabaseMigrations;
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testExample()
+    public function test_show_profile_page_response()
     {
-        $this->assertTrue(true);
+        $this->users = factory(User::class, 3)->create();
+
+        $response = $this->get('/show_profile/'.$this->users[0]->id);
+        $response-> assertStatus(200);
+        $response = $this->get('/show_profile/'.$this->users[1]->id);
+        $response-> assertStatus(200);
+        $response = $this->get('/show_profile/'.$this->users[2]->id);
+        $response-> assertStatus(200);
+
+        while(true){
+            $randomID = rand();
+
+            if($randomID != $this->users[0]->id && $randomID != $this->users[1]->id && $randomID != $this->users[2]->id){
+                $response = $this->get('/show_profile/'.$this->users[]->id);
+                $response-> assertStatus(404);
+                break;
+            }
+        }
     }
 }
