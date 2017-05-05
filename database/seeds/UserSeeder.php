91136cb45bd6bef9b785010cd8533ce74f023c94
mihp26 <m.ihp@icloud.com>
2017-05-05 23:01:13 +0700

    [green] add birthday and gender field

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,28 +14,32 @@ class UserSeeder extends Seeder
     public function run()
     {
      DB::table('users')->delete();
-     $this->create_user_model("default","default@example.com",bcrypt('default'));
-     $this->create_user_model("null","null@example.com",bcrypt('null')) ;
-     $this->create_user_model("foo","foo@example.com",bcrypt('foo'));
-     $this->create_user_model("boo","boo@example.com",bcrypt('boo'));
-     $this->create_user_model_admin("admin","admin@example.com",bcrypt('admin'));
+     $this->create_user_model("default","default@example.com",bcrypt('default'),\Carbon\Carbon::now()->toDateString(), 'male');
+     $this->create_user_model("null","null@example.com",bcrypt('null'),\Carbon\Carbon::now()->toDateString(),'female') ;
+     $this->create_user_model("foo","foo@example.com",bcrypt('foo'),\Carbon\Carbon::now()->toDateString(),'male');
+     $this->create_user_model("boo","boo@example.com",bcrypt('boo'),\Carbon\Carbon::now()->toDateString(),'female');
+     $this->create_user_model_admin("admin","admin@example.com",bcrypt('admin'),\Carbon\Carbon::createFromDate('2000','1','1')->toDateString(),'other');
     }
 
-public function create_user_model($name,$email,$password) {
+public function create_user_model($name,$email,$password, $birthday, $gender) {
         $user = new User();
         $user->name = $name;
         $user->email = $email;
         $user->password = $password;
+        $user->birthday = $birthday;
+        $user->gender = $gender;
         $user->save();
   }
 
 
-public function create_user_model_admin($name,$email,$password) {
+public function create_user_model_admin($name,$email,$password, $birthday, $gender) {
         $user = new User();
         $user->name = $name;
         $user->email = $email;
         $user->is_admin = true;
         $user->password = $password;
+        $user->birthday = $birthday;
+        $user->gender = $gender;
         $user->save();
   }
 }
