142edf24fa1222c494cf187ceb16595364e05f9b
mihp26 <m.ihp@icloud.com>
2017-05-05 23:02:18 +0700

    [red] edit profile test browser

--- tests/Browser/EditProfileTest.php
+++ tests/Browser/EditProfileTest.php
@@ -4,11 +4,12 @@ namespace Tests\Browser;
 
 use App\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use function PHPSTORM_META\type;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-class RegisterTest extends DuskTestCase
+class EditProfileTest extends DuskTestCase
 {
     use DatabaseTransactions;
     use DatabaseMigrations;
@@ -21,19 +22,29 @@ class RegisterTest extends DuskTestCase
     {
 
         $this->browse(function (Browser $browser) {
-            $browser->visit('/register')
-                    ->assertPathIs('/register')
-                    ->assertTitle('Register')
+            $user = factory(User::class)->make();
+            $browser->visit('/edit_profile/'.$user->id)
+                    ->assertPathIs('/edit_profile/'.$user->id)
+                    ->assertTitle('Edit Information')
                     ->assertSee('Name')
                     ->assertSee('E-Mail Address')
                     ->assertSee('Password')
-                    ->assertSee('Confirm Password')
+                    ->assertSee('Date of Birth')
+                    ->assertSee('Gender')
+                    ->assertSee('Bio')
+                    ->assertSee('Telephone')
+                    ->assertSee('Address')
                     ->type('name', 'Rizqy Faishal')
                     ->type('email', 'rizqyfaishal27@gmail.com')
                     ->type('password', 'password')
                     ->type('password_confirmation', 'password')
-                    ->press('Button')
-                    ->assertPathIs('/home');
+                    ->type('birthday','1996-01-01')
+                    ->select('gender', 'male')
+                    ->type('bio','Hello World! my name is Rizqy Faishal')
+                    ->type('telephone', '+62812008001')
+                    ->type('address','Kampus UI Depok')
+                    ->press('Save Changes')
+                    ->assertPathIs('/show_profile/'.$user->id);
             $user = User::where('email','=','rizqyfaishal27@gmail.com')->first();
             if(is_null($user)){
                 $this->assertTrue(false);
