a979916e903596abfe95b506058abefbcfccb704
mihp26 <m.ihp@icloud.com>
2017-05-11 22:40:05 +0700

    [red] fix show & edit profile page response test

--- tests/Feature/EditProfileTest.php
+++ tests/Feature/EditProfileTest.php
@@ -16,24 +16,12 @@ class EditProfileTest extends TestCase
      */
     public function test_edit_profile_page_response()
     {
-        $this->users = factory(User::class, 3)->create();
+        $response = $this->get('/edit_profile/');
 
-        $response = $this->get('/edit_profile/'.$this->users[0]->id);
-        $response-> assertStatus(200);
-        $response = $this->get('/edit_profile/'.$this->users[1]->id);
-        $response-> assertStatus(200);
-        $response = $this->get('/edit_profile/'.$this->users[2]->id);
-        $response-> assertStatus(200);
-
-        while(true){
-            $randomID = rand();
-
-            if($randomID != $this->users[0]->id && $randomID != $this->users[1]->id && $randomID != $this->users[2]->id){
-                $response = $this->get('/edit_profile/'.$this->users[]->id);
-                $response-> assertStatus(404);
-                break;
-            }
-
+        if(is_null(Auth::user())){
+            $response->assertStatus(404);
+        } else {
+            $response->assertStatus(200);
         }
     }
 }
--- tests/Feature/ShowProfileTest.php
+++ tests/Feature/ShowProfileTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\User;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -10,7 +11,6 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ShowProfileTest extends TestCase
 {
-    use DatabaseMigrations;
     /**
      * A basic test example.
      *
@@ -18,23 +18,12 @@ class ShowProfileTest extends TestCase
      */
     public function test_show_profile_page_response()
     {
-        $this->users = factory(User::class, 3)->create();
+        $response = $this->get('/show_profile/');
 
-        $response = $this->get('/show_profile/'.$this->users[0]->id);
-        $response-> assertStatus(200);
-        $response = $this->get('/show_profile/'.$this->users[1]->id);
-        $response-> assertStatus(200);
-        $response = $this->get('/show_profile/'.$this->users[2]->id);
-        $response-> assertStatus(200);
-
-        while(true){
-            $randomID = rand();
-
-            if($randomID != $this->users[0]->id && $randomID != $this->users[1]->id && $randomID != $this->users[2]->id){
-                $response = $this->get('/show_profile/'.$this->users[]->id);
-                $response-> assertStatus(404);
-                break;
-            }
+        if(is_null(Auth::user())){
+            $response->assertStatus(404);
+        } else {
+            $response->assertStatus(200);
         }
     }
 }
