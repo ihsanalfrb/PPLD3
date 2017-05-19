557c07e51a1717d6a301a419e6a767fad58cfbd8
mihp26 <m.ihp@icloud.com>
2017-05-19 23:22:31 +0700

    [red] add thread unit test

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -9,13 +9,15 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ThreadTest extends TestCase
 {
+    use DatabaseMigrations;
+    use DatabaseTransactions;
+    
     protected $limit = 4;
 
     public function setUp()
     {
-//        parent::setUp();
-//        $this->user = factory(UserAccount::class)->make();
-//        $this->users = factory(UserAccount::class, $this->limit)->create();
+        parent::setUp();
+        $this->thread = factory(Thread::class)->create();
     }
 
     /**
@@ -23,36 +25,53 @@ class ThreadTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
-    {
-        $this->assertTrue(true);
+    public function test_database_has_column_nama_thread(){
+        $this->assertDatabaseHas('forum_threads', [
+            'nama_thread' => $this->thread->nama_thread
+        ]);
     }
 
-//    public function test_a_model_batik_has_a_nama_batik_attribute()
-//    {
-//        $this->assertArrayHasKey('nama_batik',$this->batik->getAttributes());
-//    }
-//
-//    public function test_a_model_batik_has_a_makna_batik_attribute()
-//    {
-//        $this->assertArrayHasKey('makna_batik',$this->batik->getAttributes());
-//    }
-//
-//    public function test_a_model_batik_has_a_sejarah_batik_attribute()
-//    {
-//        $this->assertArrayHasKey('sejarah_batik',$this->batik->getAttributes());
-//    }
-//
-//    public function test_a_model_batik_has_a_asal_daerah_attribute()
-//    {
-//        $this->assertArrayHasKey('asal_daerah',$this->batik->getAttributes());
-//    }
-//
-//    public function test_list_of_batiks_has_array_size_match_with_limit(){
-//        $this->assertCount($this->limit, $this->batiks);
-//    }
-//
-//    public function test_a_batik_has_a_relationship_with_pola_batik(){
-//        $pola_batik = factory(PolaBatik::class)->make();
-//    }
+    public function test_database_has_column_created_by(){
+        $this->assertDatabaseHas('forum_threads', [
+            'created_by' => $this->thread->created_by
+        ]);
+    }
+
+    public function test_database_has_column_views(){
+        $this->assertDatabaseHas('forum_threads', [
+            'views' => $this->thread->views
+        ]);
+    }
+
+    public function test_database_has_column_replies(){
+        $this->assertDatabaseHas('forum_threads', [
+            'replies' => $this->thread->replies
+        ]);
+    }
+
+    public function test_database_has_column_content(){
+        $this->assertDatabaseHas('forum_threads', [
+            'content' => $this->thread->content
+        ]);
+    }
+
+    public function test_thread_has_nama_thread_attribute(){
+        $this->assertArrayHasKey('nama_thread', $this->user->getAttributes());
+    }
+
+    public function test_thread_has_created_by_attribute(){
+        $this->assertArrayHasKey('created_by', $this->user->getAttributes());
+    }
+
+    public function test_thread_has_views_attribute(){
+        $this->assertArrayHasKey('views', $this->user->getAttributes());
+    }
+
+    public function test_thread_has_replies_attribute(){
+        $this->assertArrayHasKey('replies', $this->user->getAttributes());
+    }
+
+    public function test_thread_has_content_attribute(){
+        $this->assertArrayHasKey('content', $this->user->getAttributes());
+    }
 }
