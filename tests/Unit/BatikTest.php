65cd2eb824ff571a25f75e0f2f42521f1662dec0
mihp26 <m.ihp@icloud.com>
2017-05-19 23:22:07 +0700

    [red] add batik unit test

--- tests/Unit/BatikTest.php
+++ tests/Unit/BatikTest.php
@@ -13,13 +13,14 @@ class BatikTest extends TestCase
 {
 
 	use DatabaseMigrations;
+	use DatabaseTransactions;
 
 	protected $limit = 4;
 
 	public function setUp()
     {
         parent::setUp();
-        $this->batik = factory(Batik::class)->make();
+        $this->batik = factory(Batik::class)->create();
         $this->batiks = factory(Batik::class, $this->limit)->create();
 
     }
@@ -29,31 +30,84 @@ class BatikTest extends TestCase
      *
      * @return void
      */
-    public function test_a_model_batik_has_a_nama_batik_attribute()
+    public function test_database_has_column_nama_batik(){
+        $this->assertDatabaseHas('batik', [
+            'nama_batik' => $this->batik->nama_batik
+        ]);
+    }
+
+    public function test_database_has_column_makna_batik(){
+        $this->assertDatabaseHas('batik', [
+            'makna_batik' => $this->batik->makna_batik
+        ]);
+    }
+
+    public function test_database_has_column_sejarah_batik(){
+        $this->assertDatabaseHas('batik', [
+            'sejarah_batik' => $this->batik->sejarah_batik
+        ]);
+    }
+
+    public function test_database_has_column_cluster_batik(){
+        $this->assertDatabaseHas('batik', [
+            'cluster_batik' => $this->batik->cluster_batik
+        ]);
+    }
+
+    public function test_database_has_column_asal_daerah(){
+        $this->assertDatabaseHas('batik', [
+            'asal_daerah' => $this->batik->asal_daerah
+        ]);
+    }
+
+    public function test_database_has_column_gambar_pola_batik(){
+        $this->assertDatabaseHas('batik', [
+            'gambar_pola_batik' => $this->batik->gambar_pola_batik
+        ]);
+    }
+
+    public function test_database_has_column_matriks_pola_batik(){
+        $this->assertDatabaseHas('batik', [
+            'matriks_pola_batik' => $this->batik->matriks_pola_batik
+        ]);
+    }
+
+    public function test_batik_has_nama_batik_attribute()
     {
         $this->assertArrayHasKey('nama_batik',$this->batik->getAttributes());
     }
 
-    public function test_a_model_batik_has_a_makna_batik_attribute()
+    public function test_batik_has_makna_batik_attribute()
     {
         $this->assertArrayHasKey('makna_batik',$this->batik->getAttributes());
     }
 
-    public function test_a_model_batik_has_a_sejarah_batik_attribute()
+    public function test_batik_has_sejarah_batik_attribute()
     {
         $this->assertArrayHasKey('sejarah_batik',$this->batik->getAttributes());
     }
 
-    public function test_a_model_batik_has_a_asal_daerah_attribute()
+    public function test_batik_has_cluster_batik_attribute()
+    {
+        $this->assertArrayHasKey('cluster_batik',$this->batik->getAttributes());
+    }
+
+    public function test_batik_has_asal_daerah_attribute()
     {
         $this->assertArrayHasKey('asal_daerah',$this->batik->getAttributes());
+    }
+
+    public function test_batik_has_gambar_pola_batik_attribute()
+    {
+        $this->assertArrayHasKey('gambar_pola_batik',$this->batik->getAttributes());
+    }
+
+    public function test_batik_has_matriks_pola_batik_attribute()
+    {
+        $this->assertArrayHasKey('matriks_pola_batik',$this->batik->getAttributes());
     }
 
     public function test_list_of_batiks_has_array_size_match_with_limit(){
         $this->assertCount($this->limit, $this->batiks);
     }
-
-//    public function test_a_batik_has_a_relationship_with_pola_batik(){
-//        $pola_batik = factory(PolaBatik::class)->make();
-//    }
 }
