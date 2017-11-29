8bcb55776a8a1baace69e84423f584849bbefded
Ihsan Alfarabi <ihsanalfarabi97@gmail.com>
2017-11-30 01:05:49 +0700

    test case 7, 6, 5

--- tests/Feature/PmplTest.php
+++ tests/Feature/PmplTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -9,6 +10,9 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class PmplTest extends TestCase
 {
+    use DatabaseMigrations;
+    use WithoutMiddleware;
+    
     /**
      * A basic test example.
      *
@@ -17,5 +21,50 @@ class PmplTest extends TestCase
     public function testExample()
     {
         $this->assertTrue(true);
+    }
+
+    public function test_tested_path_7(){
+        $response = $this
+            ->post(action('Auth\RegisterController@register',[
+                'name' => 'name',
+                'email' => 'email@domain.com',
+                'password' => 'password',
+                'password_confirmation' => 'p',
+                'birthday' => '2010-02-02',
+                'gender' => 'female'
+            ]));
+        $this->assertDatabaseMissing('users', [
+            'email' => 'email@domain.com'
+        ]);
+    }
+
+    public function test_tested_path_6(){
+        $response = $this
+            ->post(action('Auth\RegisterController@register',[
+                'name' => 'name',
+                'email' => 'email@domain.com',
+                'password' => '',
+                'password_confirmation' => 'password',
+                'birthday' => '2010-02-02',
+                'gender' => 'female'
+            ]));
+        $this->assertDatabaseMissing('users', [
+            'email' => 'email@domain.com'
+        ]);
+    }
+
+    public function test_tested_path_5(){
+        $response = $this
+            ->post(action('Auth\RegisterController@register',[
+                'name' => 'name',
+                'email' => '',
+                'password' => 'password',
+                'password_confirmation' => 'password',
+                'birthday' => '2010-02-02',
+                'gender' => 'female'
+            ]));
+        $this->assertDatabaseMissing('users', [
+            'email' => 'email@domain.com'
+        ]);
     }
 }
