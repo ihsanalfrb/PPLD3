a4663ac1f2d06f50d10f0a95e1e302315eb88590
Muhammad Ivan Halim Perdana <m.ihp@icloud.com>
2017-11-30 01:06:29 +0700

    okai

--- tests/Feature/PmplTest.php
+++ tests/Feature/PmplTest.php
@@ -9,6 +9,9 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class PmplTest extends TestCase
 {
+    use DatabaseMigrations;
+    use WithoutMiddleware;
+
     /**
      * A basic test example.
      *
@@ -17,5 +20,111 @@ class PmplTest extends TestCase
     public function testExample()
     {
         $this->assertTrue(true);
+    }
+
+    public function test_tested_path_7(){
+        $response = $this
+            ->post(action('Auth\RegisterController@register',[
+                'name' => 'name',
+                'email' => 'email@domain.com',
+                'password' => 'password',
+                'password_confirmation' => 'p',
+                'birthday' => '2010-02-02',
+                'gender' => 'female'
+            ]));
+        $this->assertDatabaseMissing('users', [
+            'email' => 'email@domain.com'
+        ]);
+    }
+
+    public function test_tested_path_6(){
+        $response = $this
+            ->post(action('Auth\RegisterController@register',[
+                'name' => 'name',
+                'email' => 'email@domain.com',
+                'password' => '',
+                'password_confirmation' => 'password',
+                'birthday' => '2010-02-02',
+                'gender' => 'female'
+            ]));
+        $this->assertDatabaseMissing('users', [
+            'email' => 'email@domain.com'
+        ]);
+    }
+
+    public function test_tested_path_5(){
+        $response = $this
+            ->post(action('Auth\RegisterController@register',[
+                'name' => 'name',
+                'email' => '',
+                'password' => 'password',
+                'password_confirmation' => 'password',
+                'birthday' => '2010-02-02',
+                'gender' => 'female'
+            ]));
+        $this->assertDatabaseMissing('users', [
+            'email' => 'email@domain.com'
+        ]);
+    }
+
+
+    public function test_tested_path_4(){
+        $response = $this
+            ->post(action('Auth\RegisterController@register',[
+                'name' => 'name',
+                'email' => '',
+                'password' => 'password',
+                'password_confirmation' => 'password',
+                'birthday' => '2010-02-02',
+                'gender' => 'female'
+            ]));
+        $this->assertDatabaseMissing('users', [
+            'email' => 'email@domain.com'
+        ]);
+    }
+
+    public function test_tested_path_3(){
+        $response = $this
+            ->post(action('Auth\RegisterController@register',[
+                'name' => 'name',
+                'email' => '',
+                'password' => 'password',
+                'password_confirmation' => 'password',
+                'birthday' => '2010-02-02',
+                'gender' => 'female'
+            ]));
+        $this->assertDatabaseMissing('users', [
+            'email' => 'email@domain.com'
+        ]);
+    }
+
+    public function test_tested_path_2(){
+        $response = $this
+            ->post(action('Auth\RegisterController@register',[
+                'name' => 'name',
+                'email' => '',
+                'password' => 'password',
+                'password_confirmation' => 'password',
+                'birthday' => '2010-02-02',
+                'gender' => 'female'
+            ]));
+        $this->assertDatabaseMissing('users', [
+            'email' => 'email@domain.com'
+        ]);
+    }
+
+    public function test_tested_path_1(){
+        $response = $this
+            ->post(action('Auth\RegisterController@register',[
+                'name' => 'name',
+                'email' => 'email@domain',
+                'password' => 'password',
+                'password_confirmation' => 'password',
+                'birthday' => '2010-02-02',
+                'gender' => 'female'
+            ]));
+        $this->assertDatabaseMissing('users', [
+            'email' => 'email@domain.com'
+        ]);
     }
 }
